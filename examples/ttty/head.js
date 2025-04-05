61a5acee8fef817063a2f7f136a60905a26e61ad
2nil <casalalanne@free.fr>
2025-04-05 19:48:27 +0200

    Some reorg and cleaning.

--- examples/ttty/head.js
+++
@@ -1,9 +0,0 @@
-if (typeof app_title === "function") {
-    app_title(document.title);
-    app_icon("app.ico");
-    app_size(650, 220, 1);
-
-    function exit_on_esc() {
-        if (event.keyCode === 27) app_exit();
-    }
-}
