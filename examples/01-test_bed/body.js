ece8cf03a2501f4a785061e18b0269032f88b1cc
2nil <casalalanne@free.fr>
2025-04-13 19:44:32 +0200

    Checked in by Denis from PC-Denis-2 on Sun, Apr 13, 2025  7:44:32 PM

--- examples/01-test_bed/body.js
+++ examples/01-test_bed/body.js
@@ -215,24 +215,109 @@ document.getElementById("select_file").addEventListener("click", async () => {
   document.addEventListener("keyup", exit_on_esc);
 });
 
+function compute_gcd(a, b) {
+  return b ? compute_gcd(b, a % b) : a;
+}
+// Reduce a fraction by finding the Greatest Common Divisor and dividing by it.
+function reduce(numerator, denominator) {
+  var gcd = compute_gcd(numerator, denominator);
+  return [numerator / gcd, denominator / gcd];
+}
+
+async function get_virtual_resolution() {
+  var wpi = await win.wallpapers_info();
+  var vleft, vtop, vright, vbottom, m_leftmost, m_rightmost, m_highest, m_lowest;
+  var first_iter = true;
+
+  console.log(Object.keys(wpi).length);
+  //console.log(wpi);
+
+  for (k in wpi) {
+    var cwi = wpi[k];
+    var rc = cwi["rect"];
+    // Rect[] is left, top, right, bottom
+    var cleft = parseInt(rc[0]),
+      ctop = parseInt(rc[1]),
+      cright = parseInt(rc[2]),
+      cbottom = parseInt(rc[3]);
+    //console.log(rc);
+    var width = cright - cleft;
+    var height = cbottom - ctop;
+    var [num, den] = reduce(width, height);
+    var gcd = compute_gcd(width, height);
+    println(`${k}`);
+    println(`&nbsp;coordinates: (${cleft},${ctop},${cright},${cbottom})`);
+    println(`&nbsp;actual dimensions : (${width}, ${height})`);
+    println(`&nbsp;gcd=${gcd}`);
+    println(`&nbsp;minimal integer dimensions: (${num}, ${den})`);
+
+    if (first_iter) {
+      first_iter = false;
+      m_leftmost = m_highest = m_rightmost = m_lowest = k;
+      vleft = cleft;
+      vtop = ctop;
+      vright = cright;
+      vbottom = cbottom;
+      m_lowest = k;
+    } else {
+      if (cleft < vleft) {
+        m_leftmost = k;
+        vleft = cleft;
+      }
+      if (ctop < vtop) {
+        m_highest = k;
+        vtop = ctop;
+      }
+      if (cright > vright) {
+        m_rightmost = k;
+        vright = cright;
+      }
+      if (cbottom > vbottom) {
+        m_lowest = k;
+        vbottom = cbottom;
+      }
+    }
+  }
+
+  println("Virtual screen");
+  println(`&nbsp;coordinates: (${vleft},${vtop},${vright},${vbottom})`);
+  var vwidth = vright - vleft,
+    vheight = vbottom - vtop;
+  var [vnum, vden] = reduce(vwidth, vheight);
+  var vgcd = compute_gcd(vwidth, vheight);
+  println(`&nbsp;actual dimensions : (${vwidth}, ${vheight})`);
+  println(`&nbsp;gcd=${vgcd}`);
+  println(`&nbsp;minimal integer dimensions: (${vnum}, ${vden})`);
+  println("");
+  println(`Leftmost is ${m_leftmost}`);
+  println(`Rightmost is ${m_rightmost}`);
+  println(`Highest is ${m_highest}`);
+  println(`Lowest is ${m_lowest}`);
+
+  return [vleft, vtop, vright, vbottom];
+}
+
 async function next_wallpaper_infos() {
   var owi = await win.wallpapers_info();
+  console.log(owi);
   var nw = await win.next_wallpaper();
 
   if (nw) {
     var no_wp_change = true;
+
     var to = setTimeout(() => {
-      console.log("Time ot while trying to change wallpaper");
+      console.log("Time out while trying to change wallpaper");
       no_wp_change = false;
     }, 6000);
+
     while (no_wp_change) {
       var nwi = await win.wallpapers_info();
       i = 0;
-      console.log("loop on wallpaper files per monitor");
+      //console.log("loop on wallpaper files per monitor");
       for (k in owi) {
         if (owi[k]["file"] !== nwi[k]["file"]) {
           clearTimeout(to);
-          println(`On monitor number ${i+1}, wallpaper has change from file ${owi[k]["file"]} to file ${nwi[k]["file"]}`)
+          println(`On monitor number ${i+1}, coordinates (${nwi[k]["rect"]}), wallpaper has change from file ${owi[k]["file"]} to file ${nwi[k]["file"]}`)
           no_wp_change = false;
 
         }
@@ -252,6 +337,7 @@ Arabic_alphabet = "ابتثجحخدذرزسشصضطظعغفقكلمنهوي";
 Chineese = "啊爱安暗按八把爸吧白百拜班般板半办帮包保抱报爆杯北被背备本鼻比笔避必边便遍辨变标表别病并补不部布步才材采彩菜参草层曾茶察查差产长常场厂唱车彻称成城承程吃冲虫出初除楚处川穿传船窗床创春词此次从村存错答达打大带待代单但淡蛋当党导到道的得灯等低底地第弟点典电店掉调丁定冬东懂动都读独度短断段对队多朵躲饿儿而耳二发乏法反饭范方房防访放非飞费分坟份风封夫服福府父副复富妇该改概敢感干刚钢高搞告哥歌革隔格个给跟根更工公功共狗够构姑古骨故顾固瓜刮挂怪关观官馆管惯光广规鬼贵国果过还孩海害含汉好号喝河和何合黑很恨红后候呼忽乎湖胡虎户互护花华划画化话怀坏欢环换黄回会婚活火或货获机鸡积基极及集级急几己寄继际记济纪技计季家加假架价间简见建健件江将讲交饺脚角叫教较接街阶结节解姐介界今金斤仅紧近进尽京经精睛景静境究九酒久就旧救居局举句具据剧拒觉绝决军开看康考靠科可课刻客肯空孔口苦哭快筷块况困拉来浪劳老乐了累类冷离李里理礼立丽利历力例连联脸练凉两辆亮量谅疗料烈林零〇领另留流六龙楼路旅绿虑论落妈马吗买卖满慢忙毛么没美每门们";
 
 var func_examples = [
+  'await get_virtual_resolution()',
   'next_wallpaper_infos()',
   'nw=await win.next_wallpaper(); if (nw) println("Advanced to next wallpaper");',
   'wi=await win.wallpapers_info(); console.log(wi); for (k in wi) { println(`${k}<br/>&nbsp;id: ${wi[k]["id"]},<br/>&nbsp;file: ${wi[k]["file"]},<br/>&nbsp;rect: ${wi[k]["rect"]}`); }',
