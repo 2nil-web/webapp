d4d0d9f9d1310eb1e04c0df43c9ba9ed61fff85a
2nil <casalalanne@free.fr>
2025-05-13 19:56:22 +0200

    Checked in by Denis from PC-Denis-2 on Tue, May 13, 2025  7:56:21 PM

--- src/tail.h
+++ src/tail.h
@@ -1,6 +1,6 @@
 
-#ifndef TAIL_H
-#define TAIL_H
+#ifndef MYTAIL_H
+#define MYTAIL_H
 
 size_t linecount(std::ifstream &file);
 size_t linecount(const std::string filename);
@@ -9,5 +9,4 @@ size_t tail_from_pos(std::string filename, size_t pos, size_t total_line = 0, si
 size_t tail_once(std::string filename, size_t &total_line, bool num, const size_t start_line, size_t seek_step);
 size_t tail(std::string filename, bool poll = false, bool num = false, const size_t start_line = 10, size_t seek_step = 0);
 
-#endif /* TAIL_H */
-
+#endif /* MYTAIL_H */
