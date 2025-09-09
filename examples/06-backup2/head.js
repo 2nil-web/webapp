6f6252aca444b55b8b371b9a0348d4c3798c3aba
2nil <casalalanne@free.fr>
2025-09-09 13:42:17 +0200

    Checked in by Denis from PC-Denis-2 on Tue, Sep  9, 2025  1:42:17 PM

--- examples/06-backup2/head.js
+++ examples/06-backup2/head.js
@@ -1,10 +1,20 @@
 
-/*    CSV   : Type => 0;ToDo => 1;Source host => 2;Source directory => 3;        Destination [user@] => 4;[host:] => 5;directory => 6
-      TABLE : Type => 0;ToDo => 1;Source host => 2;Source directory => 3;👉 => 4;Destination [user@][host:]directory => 5
-                                                                         ========
-*/
-var bak_fn="backup.list.csv";
+function clean_exit() {
+  save_backup_list();
+//  if (confirm("Exit"))
+    app.exit();
+}
 
+function esc_exit() {
+  if (event.keyCode === 27) clean_exit();
+}
+
+var bak_fn = "backup.list.csv";
+var HostName = "";
+
+/*  CSV   : Type => 0;ToDo => 1;Source host => 2;Source directory => 3;        Destination [user@] => 4;[host:] => 5;directory => 6
+    TABLE : Type => 0;ToDo => 1;Source host => 2;Source directory => 3;👉 => 4;Destination [user@][host:]directory => 5
+                                                                       ======== */
 function save_backup_list() {
   var bak_text="Type;ToDo;Source host;Source directory;Destination [user@];[host:];directory\n";
   for (var r=1; r < backup_list.rows.length; r++) {
@@ -42,23 +52,101 @@ function save_backup_list() {
   fs.write(bak_fn, bak_text);
 }
 
-function clean_exit() {
-  save_backup_list();
-//  if (confirm("Exit"))
-    app.exit();
-}
-
 function select(all=true) {
   for (var r=1; r < backup_list.rows.length; r++) {
-    cell=backup_list.rows[r].cells[1];
-    var cb=cell.children[0];
-    if (all) cb.checked=true;
-    else cb.checked=!cb.checked;
+    row=backup_list.rows[r];
+    console.log(`${row.cells[2].innerText} == ${HostName}`);
+
+    // Do not modify lines of another host
+    if (row.cells[2].innerText == HostName) {
+      var cb=row.cells[1].children[0];
+      if (all) cb.checked=true;
+      else cb.checked=!cb.checked;
+    }
   }
 }
 
-function esc_exit() {
-  if (event.keyCode === 27) clean_exit();
+function sel_row (n) {
+  var e = window.event;
+  if (document.elementFromPoint(e.clientX, e.clientY).tagName == "TD") {
+    var cb=backup_list.rows[n].cells[1].children[0];
+    cb.checked=!cb.checked;
+  }
+}
+async function load_backup_list() {
+  HostName = await env.get("COMPUTERNAME");
+
+  var str = await fs.read(bak_fn);
+  csv = str.split('\n');
+  var tbl = "<table>";
+
+  csv.forEach((row, ridx) => {
+    if (row) {
+      cells = row.split(';');
+      var uhd="";
+      var notMyHost=false;
+
+      var rws="";
+      cells.forEach((cell, cidx) => {
+        if (cidx < 4) {
+          if (cidx == 1) {
+            if (ridx == 0) {
+              rws += "<td></td>";
+            } else {
+              cell=cell.toLowerCase();
+              rws += '<td><input type="checkbox"';
+              if (cell == "yes" || cell == "true" || cell == "ok" || cell == "1") rws += " checked";
+              rws += '/></td>';
+            }
+          } else {
+            if (cidx == 2 && ridx > 0 && cell != HostName) {
+              notMyHost=true;
+            }
+            rws += '<td>'+cell+"</td>";
+          }
+        } else {
+          uhd+=cell;
+
+          if (cidx == 4) {
+             rws += '<td>';
+            if (ridx > 0) {
+              rws += '&#x1F449';
+              if (cell) uhd+='@';
+            }
+
+            rws += '</td>';
+          } else {
+            if (ridx > 0 && cidx == 5 && cell) uhd+=':';
+            else if (cidx == 6) {
+              rws += "<td>"+uhd+"</td>";
+              uhd+=cell;
+            }
+          }
+        }
+      });
+
+      tbl += '<tr onclick="sel_row('+ridx+')"';
+
+      // Deactivate lines of other host
+      if (notMyHost) {
+        console.log(rws);
+        tbl += 'style="display: none;"';
+        rws=rws.replace(/<input type="checkbox" checked\/>/, '<input type="checkbox"/>');
+      }
+
+      console.log(rws);
+      tbl += '>'+rws+"</tr>";
+    }
+  });
+
+  tbl += "</table>";
+  backup_list.innerHTML = tbl;
+/*
+  console.log(tbl);
+  pretty_tbl=tbl.replace(/(<table>)/, "$1\n").replace(/(<td>)/g, "\n    $1").replace(/(<tr)/g, "  $1").replace(/(<\/tr>)/g, "\n  $1\n");
+  console.log(pretty_tbl);
+*/
+  app.on_close("clean_exit()");
 }
 
 async function eventFunc(obj, evtStr, func) {
@@ -73,76 +161,6 @@ async function eventFunc(obj, evtStr, func) {
     await new Promise(r => setTimeout(r, 600));
     document.addEventListener("keyup", esc_exit);
   });
-}
-
-async function renable_esc_exit(evt) {
-  // Re-enable esc_on_exit after a little while
-  await new Promise(r => setTimeout(r, 100));
-  document.addEventListener("keyup", esc_exit);
-}
-
-function sel_row (n) {
-  var e = window.event;
-  if (document.elementFromPoint(e.clientX, e.clientY).tagName == "TD") {
-    var cb=backup_list.rows[n].cells[1].children[0];
-    cb.checked=!cb.checked;
-  }
-}
-async function load_backup_list() {
-  var tbl = "<table>";
-  var str = await fs.read(bak_fn);
-  csv = str.split('\n');
-  csv.forEach((row, ridx) => {
-    if (row) {
-      tbl += '<tr onclick="sel_row('+ridx+')">';
-      cells = row.split(';');
-      var uhd="";
-
-      cells.forEach((cell, cidx) => {
-        if (cidx < 4) {
-          if (cidx == 1) {
-            if (ridx == 0) {
-              tbl += "<td></td>";
-            } else {
-              cell=cell.toLowerCase();
-              tbl += '<td><input type="checkbox"';
-              if (cell == "yes" || cell == "true" || cell == "ok" || cell == "1") tbl += "checked";
-              tbl += '/></td>';
-            }
-          } else
-            tbl += '<td>'+cell+"</td>";
-        } else {
-          uhd+=cell;
-
-          if (cidx == 4) {
-             tbl += '<td>';
-            if (ridx > 0) {
-              tbl += '&#x1F449';
-              if (cell) uhd+='@';
-            }
-
-            tbl += '</td>';
-          } else {
-            if (ridx > 0 && cidx == 5 && cell) uhd+=':';
-            else if (cidx == 6) {
-              tbl += "<td>"+uhd+"</td>";
-              uhd+=cell;
-            }
-          }
-        }
-      });
-      tbl += "</tr>";
-    }
-  });
-
-  tbl += "</table>";
-  backup_list.innerHTML = tbl;
-/*
-  console.log(tbl);
-  pretty_tbl=tbl.replace(/(<table>)/, "$1\n").replace(/(<td>)/g, "\n    $1").replace(/(<tr)/g, "  $1").replace(/(<\/tr>)/g, "\n  $1\n");
-  console.log(pretty_tbl);
-*/
-  app.on_close("clean_exit()");
 }
 
 if (typeof app.sysname !== "undefined") {
@@ -177,14 +195,8 @@ if (typeof app.sysname !== "undefined") {
     eventFunc(about, "click", async () => {
       await gui.msgbox("Backup tool V2, based on " + app.info);
     });
-    /*
-        about.addEventListener("click", async (e) => {
-          disable_esc_exit(e);
-          await gui.msgbox("Backup tool V2, based on "+app.info);
-          renable_esc_exit(e);
-        console.log(about.style);
-        });*/
   }
 
   window.addEventListener("load", do_load);
 }
+
